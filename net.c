036c5e8499c73d8aa28156ff57262a9a8222be6c
Tokuhira <sgsnknch@gmail.com>
2024-01-05 16:05:02 +0900

    step2 割り込み処理

--- net.c
+++ net.c
@@ -18,7 +18,7 @@ struct net_device *net_device_alloc(void) {
         errorf("memory_alloc() failure");
         return NULL;
     }
-    return dev;    
+    return dev;
 }
 
 /* NOTE: must not be call after net_run() */
@@ -60,7 +60,7 @@ static int net_device_close(struct net_device *dev) {
             return -1;
         }
     }
-    dev->flags &= -NET_DEVICE_FLAG_UP;
+    dev->flags &= ~NET_DEVICE_FLAG_UP;
     infof("dev=%s, state=%s", dev->name, NET_DEVICE_STATE(dev));
     return 0;
 }
@@ -92,6 +92,10 @@ int net_input_handler(uint16_t type, const uint8_t *data, size_t len, struct net
 int net_run(void) {
     struct net_device *dev;
 
+    if (intr_run() == -1) {
+        errorf("intr_run() failure");
+        return -1;
+    }
     debugf("open all devices...");
     for (dev = devices; dev; dev = dev->next)
         net_device_open(dev);
@@ -105,10 +109,15 @@ void net_shutdown(void) {
     debugf("close all devices...");
     for (dev = devices; dev; dev = dev->next)
         net_device_close(dev);
+    intr_shutdown();
     debugf("shutting down");
 }
 
 int net_init(void) {
+    if (intr_init() == -1) {
+        errorf("intr_init() failure");
+        return -1;
+    }
     infof("initialized");
     return 0;
 }
