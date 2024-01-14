81e6c954d0fc027a73b74b7decc56c6f8ce1d696
Tokuhira <sgsnknch@gmail.com>
2024-01-14 19:47:58 +0900

    fix

--- arp.c
+++ arp.c
@@ -9,7 +9,7 @@
 #include "ip.h"
 
 /* see https://www.iana.org/assignments/arp-parameters/arp-parameters.txt */
-#define ARP_HDR_ETHER 0x0001
+#define ARP_HRD_ETHER 0x0001
 /* NOTE: use same value as the Ethernet types */
 #define ARP_PRO_IP ETHER_TYPE_IP
 
@@ -19,7 +19,7 @@
 struct arp_hdr {
     uint16_t hrd;
     uint16_t pro;
-    uint8_t hlen;
+    uint8_t hln;
     uint8_t pln;
     uint16_t op;
 };
@@ -51,14 +51,14 @@ static void arp_dump(const uint8_t *data, size_t len) {
     flockfile(stderr);
     fprintf(stderr, "        hrd: 0x%04x\n", ntoh16(message->hdr.hrd));
     fprintf(stderr, "        pro: 0x%04x\n", ntoh16(message->hdr.pro));
-    fprintf(stderr, "        hln: %u\n", message->hdr.hlen);
+    fprintf(stderr, "        hln: %u\n", message->hdr.hln);
     fprintf(stderr, "        pln: %u\n", message->hdr.pln);
     fprintf(stderr, "         op: %u (%s)\n", ntoh16(message->hdr.op), arp_opcode_ntoa(message->hdr.op));
     fprintf(stderr, "        sha: %s\n", ether_addr_ntop(message->sha, addr, sizeof(addr)));
     memcpy(&spa, message->spa, sizeof(spa));
     fprintf(stderr, "        spa: %s\n", ip_addr_ntop(spa, addr, sizeof(addr)));
     fprintf(stderr, "        tha: %s\n", ether_addr_ntop(message->tha, addr, sizeof(addr)));
-    memcpy(&tpa, message->tpa, sizeof(spa));
+    memcpy(&tpa, message->tpa, sizeof(tpa));
     fprintf(stderr, "        tpa: %s\n", ip_addr_ntop(tpa, addr, sizeof(addr)));
 #ifdef HEXDUMP
     hexdump(stderr, data, len);
@@ -69,13 +69,13 @@ static void arp_dump(const uint8_t *data, size_t len) {
 static int arp_reply(struct net_iface *iface, const uint8_t *tha, ip_addr_t tpa, const uint8_t *dst) {
     struct arp_ether_ip reply;
 
-    reply.hdr.hrd = hton16(ARP_HDR_ETHER);
+    reply.hdr.hrd = hton16(ARP_HRD_ETHER);
     reply.hdr.pro = hton16(ARP_PRO_IP);
-    reply.hdr.hlen = ETHER_ADDR_LEN;
+    reply.hdr.hln = ETHER_ADDR_LEN;
     reply.hdr.pln = IP_ADDR_LEN;
     reply.hdr.op = hton16(ARP_OP_REPLY);
     memcpy(reply.sha, iface->dev->addr, ETHER_ADDR_LEN);
-    memcpy(reply.spa, &(((struct ip_iface *)iface)->unicast), IP_ADDR_LEN);
+    memcpy(reply.spa, &((struct ip_iface *)iface)->unicast, IP_ADDR_LEN);
     memcpy(reply.tha, tha, ETHER_ADDR_LEN);
     memcpy(reply.tpa, &tpa, IP_ADDR_LEN);
     debugf("dev=%s, len=%zu", iface->dev->name, sizeof(reply));
@@ -93,10 +93,14 @@ static void arp_input(const uint8_t *data, size_t len, struct net_device *dev) {
         return;
     }
     msg = (struct arp_ether_ip *)data;
-    if (ntoh16(msg->hdr.hrd) != ARP_HDR_ETHER || msg->hdr.hlen != ETHER_ADDR_LEN)
+    if (ntoh16(msg->hdr.hrd) != ARP_HRD_ETHER || msg->hdr.hln != ETHER_ADDR_LEN) {
+        errorf("unsupported hardware address");
         return;
-    if (ntoh16(msg->hdr.pro) != ARP_PRO_IP || msg->hdr.pln != IP_ADDR_LEN)
+    }
+    if (ntoh16(msg->hdr.pro) != ARP_PRO_IP || msg->hdr.pln != IP_ADDR_LEN) {
+        errorf("unsupported protocol address");
         return;
+    }
     debugf("dev=%s, len=%zu", dev->name, len);
     arp_dump(data, len);
     memcpy(&spa, msg->spa, sizeof(spa));
