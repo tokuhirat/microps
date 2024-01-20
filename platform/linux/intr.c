772aa1e99949818c8dec359f93f3adde04e4060b
Tokuhira <sgsnknch@gmail.com>
2024-01-20 15:51:53 +0900

    step16 ARPタイマー

--- platform/linux/intr.c
+++ platform/linux/intr.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <signal.h>
 #include <pthread.h>
+#include <time.h>
+#include <errno.h>
 
 #include "platform.h"
 
@@ -59,12 +61,32 @@ int intr_raise_irq(unsigned int irq) {
     return pthread_kill(tid, (int)irq);
 }
 
+static int intr_timer_setup(struct itimerspec *interval) {
+    timer_t id;
+
+    if (timer_create(CLOCK_REALTIME, NULL, &id) == -1) {
+        errorf("timer_create: %s", strerror(errno));
+        return -1;
+    }
+    if (timer_settime(id, 0, interval, NULL) == -1) {
+        errorf("timer_settime: %s", strerror(errno));
+        return -1;
+    }
+    return 0;
+}
+
 static void *intr_thread(void *arg) {
+    const struct timespec ts = {0, 100000}; /* 1ms */
+    struct itimerspec interval = {ts, ts};
     int terminate = 0, sig, err;
     struct irq_entry *entry;
 
     debugf("start...");
     pthread_barrier_wait(&barrier);
+    if (intr_timer_setup(&interval) == -1) {
+        errorf("intr_timer_setup() failure");
+        return NULL;
+    }
     while (!terminate) {
         err = sigwait(&sigmask, &sig);
         if (err) {
@@ -77,6 +99,9 @@ static void *intr_thread(void *arg) {
             break;
         case SIGUSR1:
             net_softirq_handler();
+            break;
+        case SIGALRM:
+            net_timer_handler();
             break;
         default:
             for (entry = irqs; entry; entry = entry->next) {
@@ -124,5 +149,6 @@ int intr_init(void) {
     sigemptyset(&sigmask);
     sigaddset(&sigmask, SIGHUP);
     sigaddset(&sigmask, SIGUSR1);
+    sigaddset(&sigmask, SIGALRM);
     return 0;
 }
